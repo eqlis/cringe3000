74d15c0d9fe3a618e041896869abb50633d13792
Anastasia Bondarenko <ne7sgreen@gmail.com>
2022-10-30 14:47:15 +0200

    Add project

---
+++ src/main/java/com/cringe/cringe3000/Cringe3000Application.java
@@ -0,0 +1,13 @@
+package com.cringe.cringe3000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cringe3000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cringe3000Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cringe/cringe3000/Cringe3000ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cringe.cringe3000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cringe3000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
